8fdea3560e5d543b06bc0cc5a7a5c4fa031b71d1
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-02 01:13:46 +0200

    add toBottom button and add links to project section buttons and socials

--- index.js
+++ index.js
@@ -2,8 +2,4 @@ window.addEventListener("load", (event) => {
   let scrollToBottom = document.querySelector("#scroll-to-bottom");
   let pageBottom = document.querySelector("#page-bottom");
   pageBottom.scrollIntoView();
-  console.log(scrollToBottom);
-  console.log(pageBottom);
-  console.log(event.target);
-  console.log("mmm");
 });
