74bc93bdaa24403560dfc84b7c1856c4d7aa589f
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-04 15:41:14 +0200

    style

--- index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", (event) => {
-  let scrollToBottom = document.querySelector("#scroll-to-bottom");
-  let pageBottom = document.querySelector("#page-bottom");
-  pageBottom.scrollIntoView();
-});
